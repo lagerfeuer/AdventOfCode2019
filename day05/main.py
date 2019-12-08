590965d5f6b3bbb95fff7f44ec703e89a1015078
Lukas Deutz <lukas.deutz@mailfence.com>
2019-12-08 11:28:36 +0100

    Day 5: Solved

---
+++ day05/main.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python3
+
+import sys
+import io
+from contextlib import redirect_stdout
+from enum import Enum
+from os.path import dirname
+from os.path import join
+
+
+class OpCodes(Enum):
+    ADD = 1
+    MUL = 2
+    READ = 3
+    PRNT = 4
+    JMPT = 5  # jump if true
+    JMPF = 6  # jump if false
+    LT = 7  # less than
+    EQ = 8  # equals
+    HLT = 99
+
+
+def read_input(file_name):
+    """
+    Read input from file_name
+    """
+    input_list = []
+    # always open relative to current file
+    file_name = join(dirname(__file__), file_name)
+    with open(file_name, 'r') as f_in:
+        input_list = f_in.read().split(',')
+    return list(map(int, input_list))
+
+
+def dereference(mem, val, mode=1):
+    """
+    Dereference a value given the parameter mode
+    """
+    if mode == 0:
+        return mem[mem[val]]
+    if mode == 1:
+        return mem[val]
+    raise RuntimeError("Unknown parameter mode %d" % (mode))
+
+
+def run(program_in, stdin=None, print_to_stdout=True):
+    """
+    Run the intcode program
+    """
+    stdout = []
+    program = program_in.copy()
+    pc = 0
+    while pc < len(program):
+        p_mode = None
+        opcode = None
+        raw_opcode = str(program[pc]).zfill(5)
+        if int(raw_opcode) > 99:
+            p_mode = [int(x) for x in raw_opcode[:-2]]
+            opcode = OpCodes(int(raw_opcode[-2:]))
+        else:
+            p_mode = [0 for _ in range(3)]
+            opcode = OpCodes(program[pc])
+
+        # 0 parameter
+        if opcode == OpCodes.HLT:
+            return program, stdout
+
+        # 1 parameter
+        cont = False
+        op1 = dereference(program, pc + 1, mode=p_mode[2])
+        dst = program[pc + 1]
+
+        if opcode == OpCodes.READ:
+            cont = True
+            if stdin is None:
+                program[dst] = int(input())
+            else:
+                program[dst] = int(stdin.readline().strip())
+        if opcode == OpCodes.PRNT:
+            cont = True
+            stdout.append("%s\n" % op1)
+            if print_to_stdout:
+                print(op1)
+
+        if cont:
+            pc += 2
+            continue
+
+        # 2 parameter
+        op2 = dereference(program, pc + 2, mode=p_mode[1])
+        if opcode == OpCodes.JMPT:
+            cont = True
+            if op1 != 0:
+                pc = op2
+                continue
+        if opcode == OpCodes.JMPF:
+            cont = True
+            if op1 == 0:
+                pc = op2
+                continue
+        if cont:
+            pc += 3
+            continue
+
+        # 3 parameter
+        dst = program[pc + 3]
+        func = None
+
+        if opcode == OpCodes.ADD:
+            func = lambda x, y: x + y
+        if opcode == OpCodes.MUL:
+            func = lambda x, y: x * y
+        if opcode == OpCodes.LT:
+            func = lambda x, y: 1 if x < y else 0
+        if opcode == OpCodes.EQ:
+            func = lambda x, y: 1 if x == y else 0
+
+        program[dst] = func(op1, op2)
+        pc += 4
+
+    return program, stdout
+
+
+def part1():
+    """
+    Solve the puzzle (part 1), given the input in input.txt
+    """
+    program = read_input('input.txt')
+    stdin = io.StringIO('1')
+    program, out = run(program, stdin=stdin, print_to_stdout=False)
+    return out
+
+
+def part2():
+    """
+    Solve the puzzle (part 2), given the input in input.txt
+    """
+    program = read_input('input.txt')
+    stdin = io.StringIO('5')
+    program, out = run(program, stdin=stdin, print_to_stdout=False)
+    return out
+
+
+if __name__ == '__main__':
+    print("Part 1:", part1()[-1].strip())
+    print("Part 2:", part2()[-1].strip())
---
+++ day05/main_test.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+
+import unittest
+import pytest
+import io
+import os
+import sys
+
+from .main import part1, part2, run
+
+
+class Day5Test(unittest.TestCase):
+    def test_add(self):
+        prg = [1, 0, 0, 0, 99]
+        res = [2, 0, 0, 0, 99]
+        ref, _ = run(prg)
+        self.assertEqual(ref, res)
+
+    def test_mul(self):
+        prg = [2, 3, 0, 3, 99]
+        res = [2, 3, 0, 6, 99]
+        ref, _ = run(prg)
+        self.assertEqual(ref, res)
+
+    def test_mul_long(self):
+        prg = [2, 4, 4, 5, 99, 0]
+        res = [2, 4, 4, 5, 99, 9801]
+        ref, _ = run(prg)
+        self.assertEqual(ref, res)
+
+    def test_add_mul(self):
+        prg = [1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50]
+        res = [3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50]
+        ref, _ = run(prg)
+        self.assertEqual(ref, res)
+
+    def test_overwrite_hlt(self):
+        prg = [1, 1, 1, 4, 99, 5, 6, 0, 99]
+        res = [30, 1, 1, 4, 2, 5, 6, 0, 99]
+        ref, _ = run(prg)
+        self.assertEqual(ref, res)
+
+    def test_parameter_mode(self):
+        prg = [1002, 4, 3, 4, 33]
+        res = [1002, 4, 3, 4, 99]
+        ref, _ = run(prg)
+        self.assertEqual(ref, res)
+
+    def test_less_than_position_mode(self):
+        prg = [3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8]
+        stdin = io.StringIO('5')
+        _, out = run(prg, stdin=stdin)
+        self.assertEqual(int(out[-1].strip()), 1)
+        stdin = io.StringIO('8')
+        _, out = run(prg, stdin=stdin)
+        self.assertEqual(int(out[-1].strip()), 0)
+
+    def test_less_than_parameter_mode(self):
+        prg = [3, 3, 1107, -1, 8, 3, 4, 3, 99]
+        stdin = io.StringIO('5')
+        _, out = run(prg, stdin=stdin)
+        self.assertEqual(int(out[-1].strip()), 1)
+        stdin = io.StringIO('8')
+        _, out = run(prg, stdin=stdin)
+        self.assertEqual(int(out[-1].strip()), 0)
+
+    def test_equal_position_mode(self):
+        prg = [3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8]
+        stdin = io.StringIO('8')
+        _, out = run(prg, stdin=stdin)
+        self.assertEqual(int(out[-1].strip()), 1)
+        stdin = io.StringIO('7')
+        _, out = run(prg, stdin=stdin)
+        self.assertEqual(int(out[-1].strip()), 0)
+
+    def test_equal_parameter_mode(self):
+        prg = [3, 3, 1108, -1, 8, 3, 4, 3, 99]
+        stdin = io.StringIO('8')
+        _, out = run(prg, stdin=stdin)
+        self.assertEqual(int(out[-1].strip()), 1)
+        stdin = io.StringIO('7')
+        _, out = run(prg, stdin=stdin)
+        self.assertEqual(int(out[-1].strip()), 0)
+
+    def test_solution1(self):
+        out = part1()
+        self.assertEqual(int(out[-1].strip()), 7259358)
+
+    def test_solution2(self):
+        out = part2()
+        self.assertEqual(int(out[-1].strip()), 11826654)
+
+
+if __name__ == "__main__":
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    unittest.main()
