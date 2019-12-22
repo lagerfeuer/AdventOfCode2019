57159663b70b296d75bb648b9ab79f8e59ce851e
Lukas Deutz <lukas.deutz@mailfence.com>
2019-12-22 22:27:15 +0100

    Day 8: Solved

---
+++ day08/main.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+from os.path import dirname
+from os.path import join
+from collections import Counter
+
+
+class Layer:
+    def __init__(self, data, dimension):
+        self.data = list(map(int, data))
+        self.width, self.height = dimension
+
+    def dump(self):
+        return [self.data[i:i+self.width]
+                for i in range(0, len(self.data), self.width)]
+
+    def stream(self):
+        return self.data
+
+
+def read_input(file_name='input.txt'):
+    # always open relative to current file
+    file_name = join(dirname(__file__), file_name)
+    with open(file_name, 'r') as f_in:
+        return list(f_in.read().strip())
+
+
+def chunkify(data, chunk_size):
+    length = len(data)
+    return [data[i:i+chunk_size] for i in range(0, length, chunk_size)]
+
+
+def parse(data, dimension):
+    width, height = dimension
+    frame_size = width * height
+    return [Layer(frame, dimension) for frame in chunkify(data, frame_size)]
+
+
+def overlay(layers):
+    data = layers[0].stream()
+    for layer in layers[1:]:
+        data = [new if new != 2 else old
+                for old, new in zip(data, layer.stream())]
+    return data
+
+
+def part1():
+    layers = parse(read_input(), (25, 6))
+    layer = min(layers, key=lambda x: x.stream().count(0))
+    cnt = Counter(layer.stream())
+    return cnt[1] * cnt[2]
+
+
+def part2():
+    layers = parse(read_input(), (25, 6))[::-1]
+    data = overlay(layers)
+    visual = [{0: ' ', 1: '#'}[e] for e in data]
+    return '\n'.join(chunkify(''.join(visual), 25))
+
+
+if __name__ == '__main__':
+    print("Part 1:", part1())
+    print("Part 2:\n")
+    print(part2())
---
+++ day08/main_test.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import unittest
+import os
+from math import prod
+
+from .main import part1, part2
+from .main import parse, overlay
+
+
+class Day8Test(unittest.TestCase):
+    def test_layer_parse(self):
+        data = list("123456789012")
+        dim = (3, 2)
+        ref = [
+            [list(map(int, list(x))) for x in "123 456".split()],
+            [list(map(int, list(x))) for x in "789 012".split()]
+        ]
+        out = [layer.dump() for layer in parse(data, dim)]
+        self.assertEqual(ref, out)
+        ref = [list(map(int, data[i:i+prod(dim)]))
+               for i in range(0, len(data), prod(dim))]
+        out = [layer.stream() for layer in parse(data, dim)]
+        self.assertEqual(ref, out)
+
+    def test_visualize(self):
+        ref = list(map(int, "0110"))
+        data = "0222112222120000"
+        dim = (2, 2)
+        layers = parse(data, dim)
+        out = overlay(layers[::-1])
+        self.assertEqual(ref, out)
+
+    def test_solution1(self):
+        out = part1()
+        self.assertEqual(out, 1572)
+
+    # def test_solution2(self):
+    #     out = part2()
+    #     self.assertEqual(out, 8754464)
+
+
+if __name__ == "__main__":
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    unittest.main()
